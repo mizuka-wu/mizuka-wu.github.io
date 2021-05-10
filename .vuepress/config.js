c66b73a474b4e4ad6fc01f5ff1ec5502d9ce8100
水华 <mizuka.wu@outlook.com>
2021-05-10 23:53:14 +0800

    feat: 博客初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: '水华的博客', // Title for the site. This will be displayed in the navbar.
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+      // Please keep looking down to see the available options.
+    }
+  }
