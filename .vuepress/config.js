308a3b3060e3b58e58c361b2cb4e68aa63134184
mizuka.wu <mizuka.wu@outlook.com>
2021-07-02 17:50:14 +0800

    feat: 增加live2d模型

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
     title: '水华的博客', // Title for the site. This will be displayed in the navbar.
     theme: '@vuepress/theme-blog',
+    plugins: [
+        ["vuepress-plugin-pixi-live2d-display", {
+            delay: 3000,
+            model: "/live2d/kueshishui.model3.json"
+        }]
+    ],
     themeConfig: {
         sitemap: {
             hostname: 'https://www.mizuka.top',
